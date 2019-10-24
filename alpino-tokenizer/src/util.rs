5d5b98d67d2f1d2da1bb0b1bea253e7abffce8d7
Daniël de Kok <me@danieldk.eu>
2019-10-24 06:56:52 +0200

    Fix clippy warnings

--- alpino-tokenizer/src/util.rs
+++ alpino-tokenizer/src/util.rs
@@ -1,6 +1,6 @@
 pub fn str_to_tokens(tokenized: &str) -> Vec<Vec<String>> {
     tokenized
-        .split("\n")
-        .map(|sent| sent.split(" ").map(ToOwned::to_owned).collect::<Vec<_>>())
+        .split('\n')
+        .map(|sent| sent.split(' ').map(ToOwned::to_owned).collect::<Vec<_>>())
         .collect::<Vec<_>>()
 }
