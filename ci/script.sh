15a92823ef438f1a02548316a1eda408ec938224
Daniël de Kok <me@danieldk.eu>
2019-10-24 08:30:54 +0200

    clippy?

--- ci/script.sh
+++ ci/script.sh
@@ -5,13 +5,8 @@ set -ex
 # Fail early on formatting errors.
 if [ "$TRAVIS_RUST_VERSION" = "stable" ]; then
   cargo fmt --all -- --check
+  cargo clippy -- -D warnings
 fi
 
 cargo check
 cargo test
-
-if [ "$TRAVIS_RUST_VERSION" = "stable" ]; then
-  cargo clippy -- -D warnings
-fi
-
-
